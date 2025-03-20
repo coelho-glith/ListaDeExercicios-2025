71b0ddd277ec01d25860072482ac78b1c3aefd2e
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 15:03:12 -0300

    Exercicio 5

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva o seu salário base: ");
+            double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Escreva o total de vendas: ");
+            int totaldeVendas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Escreva a porcentagem de Comissão que você ganha por venda: ");
+            double porcentagem = Convert.ToDouble(Console.ReadLine());
+
+            double porcentagemConvertida = porcentagem / 100;
+
+            double salarioTotal = salarioBase + totaldeVendas + (totaldeVendas * porcentagemConvertida);
+
+            Console.Write($"O salário total do vendedor é de {salarioTotal} ");
+
+            Console.ReadLine();
+        }
+    }
+}
