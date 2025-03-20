405d070002b307f4a8b90b395228fe269ed17fda
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 14:14:06 -0300

    Exercicio 2

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double raio = 3.1;
+            double altura = 10;
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é: {volume} cm3 ");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
