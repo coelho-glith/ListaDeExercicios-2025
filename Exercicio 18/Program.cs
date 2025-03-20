bd5e17d37970c0f3241fe371132b393d2bb0d40e
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 17:44:21 -0300

    Exercicio 18

---
+++ Exercicio 18/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
