5f392db8135121d9c1b0c4ee87275c3073149309
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 17:34:53 -0300

    Exercicio 17

---
+++ Exercicio 17/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor para letra A: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para letra B: ");
+            double b = double.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                double c = a + b;
+
+                Console.WriteLine($"O valor de C é: {c}");
+            }
+            else
+            {
+                Console.WriteLine("Os valores de A e B são diferentes");
+            }
+        }
+    }
+}
