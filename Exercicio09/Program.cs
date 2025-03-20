47b13ba962972a4b133311001238bf517cc493e0
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 15:54:25 -0300

    Exercicio 9

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o comprimento do terreno: ");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira a largura do terreno: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            decimal area = Convert.ToDecimal(comprimento * largura);
+
+            Console.WriteLine($"A área do terreno é {area}");
+
+        }
+    }
+}
