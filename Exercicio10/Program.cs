38aae3d6a76205d4cfad33a1bccb5352c44af782
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 16:05:25 -0300

    Exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("coloque a quantidade de pães que vendeu:");
+            decimal pao = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("coloque a quantidade de broas que vendeu:");
+            decimal broa = Convert.ToDecimal(Console.ReadLine());
+
+            decimal total = (pao * 0.12m) + (broa * 1.50m);
+
+            Console.WriteLine($"O total de vendas é: {total}");
+
+            decimal lucro = total * 0.1m;
+
+            Console.WriteLine($"deve guardar {lucro} na conta poupança");
+
+        }
+    }
+}
