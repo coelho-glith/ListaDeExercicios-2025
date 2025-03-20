a91ca42c17b9ac4f6c2321a425e2617788e9c585
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 17:51:25 -0300

    Exercicio 20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira um número para colocar na tabuada:");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+    }
+}
