59f83d9ddaf9fafb20e3c16a8c4de4a19e822038
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 16:12:19 -0300

    Exercicio 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque o seu nome aqui:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Coloque a sua idade aqui:");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            int Calculo = idade * 365;
+
+            Console.WriteLine($"{nome}, você já viveu {Calculo} dias");
+
+        }
+    }
+}
