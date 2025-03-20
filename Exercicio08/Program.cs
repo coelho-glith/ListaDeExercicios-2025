dc883c1226a1c162cba9d03934039dcb484835a8
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 15:47:45 -0300

    Exercicio 8

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número para ver se é primo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int divisor1 = numero / 1;
+            int divisor2 = numero / numero;
+
+            if (divisor1 == 1 && divisor2 == 1)
+            {
+                Console.WriteLine("O número é primo");
+            }
+            else
+            {
+                Console.WriteLine("O número não é primo");
+            }
+
+        }
+    }
+}
