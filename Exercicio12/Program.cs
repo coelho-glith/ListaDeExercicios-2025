201eaaf0c99ccac1c258f6b0694afd046933c20f
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 16:24:12 -0300

    Exercicio 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque o seu salário aqui: ");
+            decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+            decimal aumento = salario * 0.15m;
+            decimal impostos = salario * 0.08m;
+            decimal salarioFinal = salario + aumento - impostos;
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"O seu salário inicial é: {salario}");
+            Console.WriteLine($"O seu salário com aumento é: {salario + aumento}");
+            Console.WriteLine($"O seu salário final é: {salarioFinal}");
+            Console.WriteLine("-----------------------------");
+
+        }
+    }
+}
