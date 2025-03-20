e695bd6ec690388d5fed7695c6eb2ba7269b7954
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 15:36:59 -0300

    Exercicio07

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Coloque a primeira nota do aluno aqui: ");
+            double primeiraNota = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque a segunda nota do aluno aqui: ");
+            double segundaNota = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque a terceira nota do aluno aqui: ");
+            double terceiraNota = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque a quarta nota do aluno aqui: ");
+            double quartanota = Convert.ToDouble(Console.ReadLine());
+
+            Math.
+
+        }
+    }
+}
---
+++ Exercicio07/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a nota da primeira prova: ");
+            decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Insira o peso da primeira prova: ");
+            decimal peso1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Insira a nota da segunda prova: ");
+            decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Insira o peso da segunda prova: ");
+            decimal peso2 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal mediaPonderada = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+
+            Console.WriteLine($"A média ponderada do aluno é de {mediaPonderada}");
+
+        }
+    }
+}
