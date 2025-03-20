4bdea24a4a92517a6a7cb4dec32f31d43c19108c
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 17:20:08 -0300

    Exercicio 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número é par");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar");
+            }
+
+
+        }
+    }
+}
