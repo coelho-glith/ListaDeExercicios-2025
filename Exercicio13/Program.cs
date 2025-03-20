583faa2fc7b157154e27122cfe4cb734736484be
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 16:35:27 -0300

    Exercicio 13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Coloque o valor da letra A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque o valor da letra B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque o valor da letra C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (a + b < c)
+            {
+                Console.WriteLine("A + B é menor que C");
+            }
+            else
+            {
+                Console.WriteLine("A + B  que C");
+            }
+
+        }
+    }
+}
