772ef13e5287d1366cdb0a583f4f55707ddb5f39
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 14:45:10 -0300

    Exercicio4

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva A temperatura em Celcius: ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+
+            double Fahrenheit = (celcius * 1.8) + 32;
+
+            Console.WriteLine($"A temperatura em Fahrenheit è: {Fahrenheit}");
+
+            Console.ReadLine();
+        }
+    }
+}
