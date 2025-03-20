5871139fef3334488442456d32ca1c0870daab10
coelho-glith <coelho.soldado7@gmail.com>
2025-03-20 17:04:59 -0300

    Exercicio 14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] decrescente = new int[3];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.Write($"Digite o {i + 1}º valor: ");
+                decrescente[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Array.Sort(decrescente);
+            Array.Reverse(decrescente);            
+
+            Console.WriteLine($"Os números em ordem decrescente é: {string.Join(", ", decrescente)}");
+
+        }
+    }
+}
