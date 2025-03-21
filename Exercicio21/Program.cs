7820ba2280f910c6e4ee0a0250df3fa6106e98f8
coelho-glith <coelho.soldado7@gmail.com>
2025-03-21 05:58:56 -0300

    Exercicio21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor A: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            int valorTotal = valor;
+
+            Console.Write($"{valor}! = {valor}");
+            for (int i = valor - 1; i > 0; i--)
+            {
+                valorTotal *= i;
+                Console.Write($" X {i} ");
+            }
+
+            Console.Write($"= {valorTotal}");
+        }
+    }
+}
