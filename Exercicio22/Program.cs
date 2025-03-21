9a34359bad6c7f878752799fef07c9b876698e59
coelho-glith <coelho.soldado7@gmail.com>
2025-03-21 13:42:31 -0300

    Exercicio22

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Escreva um número:");
+           int numero = int.Parse(Console.ReadLine());
+
+           int valorAtual = 0;
+           int proximoValor = 1;
+           
+           for (int i = 0; i <= numero; i++)
+            {
+
+                int soma = valorAtual + proximoValor;
+                Console.WriteLine(soma);
+
+                valorAtual = proximoValor;
+                proximoValor = soma;
+            }
+        }
+    }
+}
